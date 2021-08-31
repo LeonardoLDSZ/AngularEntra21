8a6018547b75a15c1f5300c05af5883592712f64
LeonardoLDSZ <leonardodavidesouza@gmail.com>
2021-08-30 22:04:45 -0300

    monday's commit

--- beer-app/src/app/app.module.ts
+++ beer-app/src/app/app.module.ts
@@ -1,18 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
+import { DatabeerService } from './databeer.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule    
   ],
-  providers: [],
+  providers: [DatabeerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ beer-app/src/app/beer.ts
@@ -0,0 +1,11 @@
+export class Beer {
+
+    id : number = 0;
+    name: string  ='';
+    tagline: string = '';
+    first_brewed: string = '';
+    description: string = '';
+    image_url: string = '';
+    abv: number = 0;
+    ibu: number = 0;
+}
